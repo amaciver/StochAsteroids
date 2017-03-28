da504c7d4d984afe6d3c213cb4fbcad56330fe04
Andrew MacIver <maciver.andrew@gmail.com>
2017-03-28 11:25:32 -0700

    made asteroid and game es6

--- stochasteroids.js
+++ stochasteroids.js
@@ -1,8 +1,8 @@
 const Game = require('./lib/game.js');
 const GameView = require('./lib/game_view.js');
 const Util = require('./lib/utils.js');
-const MovingObject = require('./lib/moving_object.js');
-const Asteroid = require('./lib/asteroid.js');
+// const MovingObject = require('./lib/moving_object.js');
+// const Asteroid = require('./lib/asteroid.js');
 
 // const AudioTest = require('./lib/audio_test.js');
 const Drawer = require('./lib/drawer.js');
@@ -10,8 +10,8 @@ const Drawer = require('./lib/drawer.js');
 import Audio from './lib/audio.js';
 
 window.Game = Game;
-window.Asteroid = Asteroid;
-window.MovingObject = MovingObject;
+// window.Asteroid = Asteroid;
+// window.MovingObject = MovingObject;
 window.GameView = GameView;
 window.Util = Util;
 
