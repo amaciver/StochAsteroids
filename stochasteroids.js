b7d9f40bb76e977f0aa752fc5f29d435f3b506ab
Andrew MacIver <maciver.andrew@gmail.com>
2017-03-30 10:27:53 -0700

    Add sound selection and pause when drawing

--- stochasteroids.js
+++ stochasteroids.js
@@ -19,28 +19,11 @@ window.GameView = GameView;
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  const sources = [
-    "sounds/dink.wav",
-    "sounds/epiano/epiano_c3.wav",
-    "sounds/epiano/epiano_d3.wav",
-    "sounds/epiano/epiano_e3.wav",
-    "sounds/epiano/epiano_f3.wav",
-    "sounds/epiano/epiano_g3.wav",
-    "sounds/epiano/epiano_a3.wav",
-    "sounds/epiano/epiano_b3.wav",
-    "sounds/epiano/epiano_c4.wav",
-    "sounds/epiano/epiano_d4.wav",
-    "sounds/epiano/epiano_e4.wav",
-    "sounds/epiano/epiano_f4.wav",
-    "sounds/epiano/epiano_g4.wav",
-    "sounds/epiano/epiano_a4.wav",
-    "sounds/epiano/epiano_b4.wav"
-  ]
+
   const canvasEl = document.getElementsByTagName("canvas")[0];
   const ctx = canvasEl.getContext("2d");
-  let audio = new Audio(sources);
-  audio.init();
-  audio.loadSounds();
+  let audio = new Audio();
+
   // setTimeout(() => audio.playSound(0), 2000);
   const gv = new GameView(ctx, audio);
   gv.init();
